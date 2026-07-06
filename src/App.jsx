9f725e81c2b0eb9fdd7d43d3e9174a636789f12a
Soyeb Ahmed <soyebahmed@gmail.com>
2026-07-06 16:06:24 +0530

    ai assistance files

---
+++ src/App.jsx
@@ -0,0 +1,154 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+import ReactMarkdown from "react-markdown";
+import GetResponse from "./ai-response";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
+
+function App() {
+  const [inputPrompt, respponsePrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function getResponse(formData) {
+    const prompt = formData.get("input-query");
+    try {
+      const response = await GetResponse(prompt);
+      if (response) setLoading(false);
+      respponsePrompt(response);
+    } catch (error) {
+      respponsePrompt(error);
+    }
+  }
+
+  return (
+    <>
+      {" "}
+      return <h1>PDF Reader</h1>;
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Nios AI Assistant</h1>
+          <p>some data</p>
+        </div>
+
+        <section className="popup-description">
+          {loading ? (
+            <p>....Loading</p>
+          ) : (
+            <ReactMarkdown>{inputPrompt}</ReactMarkdown>
+          )}
+        </section>
+
+        {/* <ReactMarkdown children= /> */}
+
+        <form action={getResponse}>
+          <input type="text" name="input-query" className="counter" />
+          <button
+            type="submit"
+            className="counter"
+            onClick={() => setLoading(true)}
+          >
+            Ask me!
+          </button>
+        </form>
+      </section>
+      <div className="ticks"></div>
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/ai-response.js
@@ -0,0 +1,81 @@
+import OpenAI from "openai";
+
+import * as pdfjsLib from "pdfjs-dist";
+
+// Required
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
+
+let text = "";
+
+const loadingTask = pdfjsLib.getDocument({
+  url: "/Academic_Prospectus_2026-27.pdf",
+});
+
+const pdf = await loadingTask.promise;
+
+console.log("Pages:", pdf.numPages);
+const pages = [];
+
+for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+  const page = await pdf.getPage(pageNum);
+
+  const content = await page.getTextContent();
+
+  const text = content.items.map((item) => item.str).join(" ");
+
+  pages.push({
+    page: pageNum,
+    text,
+  });
+}
+
+const newClient = new OpenAI({
+  apiKey: import.meta.env.VITE_API_KEY,
+  baseURL: import.meta.env.VITE_API_URL,
+  dangerouslyAllowBrowser: true,
+});
+
+//setup message
+
+async function GetResponse(prompt) {
+  const message = [
+    {
+      role: "user",
+      content: prompt,
+    },
+    {
+      role: "system",
+      content: `
+You are an AI assistant for the NIOS Academic Prospectus.
+
+Answer the user's question ONLY using the provided context.
+
+Context:
+${text}
+
+Rules:
+- Use only the information in the context.
+- Keep the answer under 200 words.
+`,
+    },
+  ];
+
+  try {
+    const response = await newClient.chat.completions.create({
+      model: import.meta.env.VITE_API_MODEL,
+      messages: message,
+      max_tokens: 500,
+    });
+
+    return response.choices[0].message.content;
+  } catch (error) {
+    return "Latest error: " + error;
+  }
+}
+
+export default GetResponse;
+
+//return response.choices[0].message.content;
